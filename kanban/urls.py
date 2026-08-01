c297710a25e4c17cb1cdac99281e2e08a4785bc2
justine-neema <j.neema@alustudent.com>
2026-08-01 10:48:56 +0200

    chore(settings): include Render host in ALLOWED_HOSTS

--- kanban/urls.py
+++ kanban/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.views.generic import RedirectView
 from rest_framework.routers import DefaultRouter
 from .views import *
 
@@ -17,5 +18,6 @@ router.register(r'activities', ActivityLogViewSet)
 router.register(r'dashboard', DashboardViewSet, basename='dashboard')
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/', permanent=False), name='api-root-redirect'),
     path('api/', include(router.urls)),
 ]
