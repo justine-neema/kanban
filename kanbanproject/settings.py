fa6760e0527dd232fb27c760f0de3300ba10ff52
justine-neema <j.neema@alustudent.com>
2026-08-05 14:48:51 +0200

    remove djo

--- kanbanproject/settings.py
+++ kanbanproject/settings.py
@@ -59,7 +59,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
-    'djo',
+    # 'djo',
     'django_extensions',
     
     # Local apps
